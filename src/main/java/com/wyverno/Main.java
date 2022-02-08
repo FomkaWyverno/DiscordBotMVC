f406d86653e5b75c8bec17f2985e30daa8ee7ff1
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2022-02-08 23:02:08 +0200

    add dependencies

--- src/main/java/com/wyverno/Main.java
+++ src/main/java/com/wyverno/Main.java
@@ -1,4 +1,13 @@
 package com.wyverno;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.security.auth.login.LoginException;
+
 public class Main {
+    public static void main(String[] args) throws LoginException {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Hello world");
+    }
 }
