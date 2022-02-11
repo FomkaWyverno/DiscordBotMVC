aea2fba0e52c08b4b5eb5e8b3546880bbb50d761
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2022-02-11 18:58:48 +0200

    added to main method, starting JDA

--- src/main/java/com/wyverno/view/View.java
+++ src/main/java/com/wyverno/view/View.java
@@ -5,27 +5,46 @@ import net.dv8tion.jda.api.JDABuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
 
 public class View {
-    public static void main(String[] args) throws LoginException, InterruptedException {
-        Logger logger = LoggerFactory.getLogger(View.class);
-        logger.info("Starting bot.");
-        JDA jda = null;
-        try {
-            jda = JDABuilder.createDefault("token").build();
-        } catch (LoginException e) {
-            logger.error("Error loging Stacktrace:",e);
-        }
 
-        logger.info("Started bot.");
-        logger.trace("Run");
-        Thread.sleep(1000);
-        logger.error("finish bot.");
-        jda.shutdown();
+    public static final JDA JDA;
+    public static final Logger logger = LoggerFactory.getLogger(View.class);
+
+    static {
+        JDA = null;
+        try {
+
+        } catch (Throwable throwable) {
+            logger.error("Error while loading JDA");
+        }
     }
 
-    public static int getInt() {
-        return 5;
+    public static void main(String[] args) {
+        try {
+            Properties config = new Properties();
+            logger.trace("Loading config...");
+            try(BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(
+                                    View.class.getClassLoader().
+                                            getResource("config.properties").getFile()
+                            )))) {
+                logger.trace("Reading config");
+                config.load(reader);
+            }
+            logger.trace("Reading token for JDA from config");
+            JDABuilder JdaBuilder = JDABuilder.createDefault(config.getProperty("DISCORD_API_TOKEN"));
+            logger.info("Starting bot");
+
+        } catch (Throwable t) {
+            logger.error("FATAL ERROR STACKTRACE:",t);
+        }
+
+
     }
 }
