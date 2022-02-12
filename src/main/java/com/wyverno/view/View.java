618900c32a503c5c826c2cc5808ada534a315eaa
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2022-02-12 20:58:41 +0200

    Delegated methods from view to model

--- src/main/java/com/wyverno/view/View.java
+++ src/main/java/com/wyverno/view/View.java
@@ -1,52 +1,19 @@
 package com.wyverno.view;
 
+import com.wyverno.model.Model;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;;import java.io.*;
-import java.util.Properties;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class View {
 
     public static final Logger logger = LoggerFactory.getLogger(View.class);
 
-    private static JDA JDA = null;
-
     public static void main(String[] args) throws InterruptedException {
-        getJDA();
-        Thread.sleep(5000);
-        stopJDA();
-    }
-
-    public static JDA getJDA() { // Pattern Singleton
-        if (JDA != null) {
-            return JDA;
-        } else {
-            try {
-                logger.trace("Loading config.properties");
-                String fileConfig = View.class.getClassLoader().getResource("config.properties").getFile();
-                Properties properties = new Properties();
-                try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileConfig)))) {
-                    properties.load(reader);
-                    logger.trace("Loaded config.properties");
-                }
-                logger.trace("Reading token from config");
-                JDABuilder builder = JDABuilder.createDefault(properties.getProperty("DISCORD_API_TOKEN"));
-                logger.trace("Read token from config");
-
-                logger.info("Starting bot");
-                JDA = builder.build();
-                logger.info("Started bot");
-            } catch (Throwable throwable) {
-                logger.error("FATAL ERROR STACKTRACE:",throwable);
-            }
-            return JDA;
-        }
-    }
-
-    private static void stopJDA() {
-        logger.info("Stopping bot...");
-        getJDA().shutdown();
-        logger.info("Stopped bot");
+        JDA jda = Model.getJDA();
+        Thread.sleep(TimeUnit.SECONDS.toMillis(3));
+        Model.stopJDA();
     }
 }
