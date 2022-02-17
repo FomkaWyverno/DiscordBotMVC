bf79332c4422b3bb8ee31d23ead320bfb3f0ea21
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2022-02-17 21:39:28 +0200

    Implementation acceptMessageEvent

--- src/main/java/com/wyverno/model/Model.java
+++ src/main/java/com/wyverno/model/Model.java
@@ -1,12 +1,18 @@
 package com.wyverno.model;
 
 import com.wyverno.view.View;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class Model {
 
-    private final View view;
+    private final View VIEW;
 
     public Model(View view) {
-        this.view = view;
+        this.VIEW = view;
+    }
+
+    public void acceptMessageEvent(MessageReceivedEvent event) { // Принимаем и обрабатываем ивент
+        String message = event.getMessage().getContentRaw();
+        VIEW.sendMessageToTextChannel(event.getChannel(),message); // Отправляем Вюшке сообщении
     }
 }
