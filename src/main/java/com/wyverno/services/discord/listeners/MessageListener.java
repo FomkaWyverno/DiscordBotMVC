6db9533bdc1d03e0d06f8a6330e00cf6083a9e2b
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2022-02-17 21:43:57 +0200

    Add listener for all text chats

--- src/main/java/com/wyverno/services/discord/listeners/MessageListener.java
+++ src/main/java/com/wyverno/services/discord/listeners/MessageListener.java
@@ -1,13 +1,32 @@
-package com.wyverno.services.discord;
+package com.wyverno.services.discord.listeners;
 
+import com.wyverno.patterns.observer.Observable;
+import com.wyverno.patterns.observer.Observer;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MessageListener extends ListenerAdapter {
 
+    private static final Logger logger = LoggerFactory.getLogger(MessageListener.class);
+
+    private final Observable observable = new Observable();
+
+    public void addObserver(Observer observer) {
+        this.observable.addObserver(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        this.observable.removeObserver(observer);
+    }
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-
+        logger.debug(String.format("Message Event [%s/%s]: %s",event.getGuild().getName(),
+                                                                event.getAuthor().getName(),
+                                                                event.getMessage().getContentRaw()));
+        observable.notifyFromObservableObservers(this,event); // Оповещаем наблюдателя об ивенте
     }
 }
