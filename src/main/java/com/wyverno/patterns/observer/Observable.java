26832290a86efff4db60a0a53244c259196ae662
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2022-02-17 21:41:09 +0200

    add folder for pattern, and add pattern Observer

--- src/main/java/com/wyverno/patterns/observer/Observable.java
+++ src/main/java/com/wyverno/patterns/observer/Observable.java
@@ -3,7 +3,7 @@ package com.wyverno.patterns.observer;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class ObservableImpl {
+public class Observable {
 
     private List<Observer> observerList;
 
@@ -16,11 +16,23 @@ public abstract class ObservableImpl {
         observerList.remove(observer);
     }
 
+    public void notifyObservers() {
+        notifyObservers(null);
+    }
+
     public void notifyObservers(Object arg) {
+        notifyFromObservableObservers(this,arg);
+    }
+
+    public void notifyFromObservableObservers(Object observableObject) {
+        notifyFromObservableObservers(observableObject,null);
+    }
+
+    public void notifyFromObservableObservers(Object observableObject,Object arg) {
         if (observerList == null || observerList.size() == 0) return;
 
         for (Observer o : observerList) {
-            o.update(this, arg);
+            o.update(observableObject, arg);
         }
     }
 }
--- src/main/java/com/wyverno/patterns/observer/Observer.java
+++ src/main/java/com/wyverno/patterns/observer/Observer.java
@@ -1,2 +1,5 @@
-package com.wyverno.patterns.observer;public interface Observer {
+package com.wyverno.patterns.observer;
+
+public interface Observer {
+    void update(Object observable, Object o);
 }
